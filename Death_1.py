bc7c356b6aa69ccc92064d0d4f7f9cd60e35fc67
GrandFox18 <data828aop@gmail.com>
2024-09-13 17:02:47 +0530

    Update and rename venom.py to Death_1.py

--- Death_1.py
+++ Death_1.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7451198481:AAEH7UYIS2Kp2DZbvJCKPXwr6h04xnc_Sy0'
+ADMIN_ID = '5870168842'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
